5a0ec50ad9fb8fc5f9a059ecbec6d93159cfa08a
shiba mitsue <mitue.okamoto@gmail.com>
2020-04-13 00:13:51 +0900

    fix: タイムラインのitemの追加

---
+++ components/TimeLineItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-card
+    @click="sendTip"
+    class="d-flex ma-2"
+    flat
+  >
+    <v-card-text class="d-flex flex-row">
+      <v-avatar size="40" class="mr-4">
+        <v-img :src="item.to.img" />
+      </v-avatar>
+      <div>
+        <div>
+          <nuxt-link
+            :to="`?uid=${item.from.uid}`"
+            class="font-weight-black"
+          >
+            {{ item.from.name }}
+          </nuxt-link>
+          さんから
+          <nuxt-link
+            :to="`?uid=${item.to.uid}`"
+            class="font-weight-black"
+          >
+            {{ item.to.name }}
+          </nuxt-link>
+          さんへ
+          <span class="font-weight-black">
+            {{ item.point }}
+          </span>
+          ポイント贈られました！
+        </div>
+        <div>
+          {{ item.comment }}
+        </div>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
--- pages/timeline.vue
+++ pages/timeline.vue
@@ -1,10 +1,58 @@
 <template>
   <v-container>
-    test
+    <template v-for="(itme, i) in itmes">
+      <TimeLineItem
+        :item="itme"
+        :key="i"
+      />
+      <v-divider :key="i" />
+    </template>
   </v-container>
 </template>
+
 <script>
+import TimeLineItem from '@/components/TimeLineItem'
+
 export default {
-  layout: 'user'
+  layout: 'user',
+  components: {
+    TimeLineItem
+  },
+  computed: {
+    itmes () {
+      return [
+        {
+          id: 1,
+          to: {
+            uid: 54,
+            name: '三浦大知',
+            img: 'https://i.pravatar.cc/160?img=2'
+          },
+          from: {
+            uid: 2,
+            name: '山下智久',
+            img: 'https://i.pravatar.cc/160?img=1'
+          },
+          point: 39,
+          comment: 'ピアボーナスサービス'
+        },
+        {
+          id: 2,
+          to: {
+            uid: 2,
+            name: '山下智久',
+            img: 'https://i.pravatar.cc/160?img=1'
+          },
+          from: {
+            uid: 54,
+            name: '三浦大知',
+            img: 'https://i.pravatar.cc/160?img=2'
+          },
+          point: 39,
+          comment: 'ピアボーナスサービス'
+        }
+      ]
+    }
+  }
 }
 </script>
