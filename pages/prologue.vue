a19be67ac229722ee13580a0dd71f7cc605bec76
shiba mitsue <mitue.okamoto@gmail.com>
2020-04-12 18:54:21 +0900

    feat: add page prologue

---
+++ pages/prologue.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <div v-html="prologue" />
+  </v-container>
+</template>
+
+<script>
+import prologue from '@/assets/md/prologue.md'
+export default {
+  layout: 'user',
+  computed: {
+    prologue () {
+      return prologue
+    }
+  }
+}
+</script>
