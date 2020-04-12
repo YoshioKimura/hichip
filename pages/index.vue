d44d0d22f6e8d8d78231a47789346e6fc4050200
cha1ra <cha1ra.garnet@gmail.com>
2020-04-12 17:09:00 +0900

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <p class="text-center">
+          ここから始まるhi-chip {{ $moment().format("YYYY-MM-DD") }}
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
