bd6b8bb64a8fc531d01eba5f4643bc1309b29d1e
shiba mitsue <mitue.okamoto@gmail.com>
2020-04-12 18:39:17 +0900

    feat: add user list  page

---
+++ components/UserListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-list-item>
+    <v-list-item-avatar size="80">
+      <v-img :src="user.img" />
+    </v-list-item-avatar>
+    <v-list-item-content>
+      <v-list-item-subtitle v-text="user.name" />
+      <button @click="sendTip">チップを贈る</button>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  methods: {
+    sendTip () {
+      alert(this.user.id)
+    }
+  }
+}
+</script>
---
+++ pages/userlist.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-list>
+      <template v-for="(user, i) in users">
+        <UserListItem :user="user" :key="i" />
+      </template>
+    </v-list>
+  </v-container>
+</template>
+
+<script>
+import UserListItem from '@/components/UserListItem'
+export default {
+  layout: 'user',
+  components: {
+    UserListItem
+  },
+  computed: {
+    users () {
+      return [
+        {
+          id: 54,
+          name: '山下智久',
+          img: 'https://i.pravatar.cc/80?img=1'
+        },
+        {
+          id: 2,
+          name: '三浦春馬',
+          img: 'https://i.pravatar.cc/80?img=2'
+        }
+      ]
+    }
+  }
+}
+</script>
