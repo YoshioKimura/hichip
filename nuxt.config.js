30e730ddbcdf7568c320253b45a1a0608adc08fb
shiba mitsue <mitue.okamoto@gmail.com>
2020-04-16 22:07:12 +0900

    feat: ログイン周り。リダイレクトの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  router: {
+    middleware: ['auth']
+  },
   /*
   ** Customize the progress-bar color
   */
@@ -98,6 +101,12 @@ export default {
   },
 
   auth: {
+    redirect: {
+      login: '/login',
+      logout: '/login',
+      callback: false,
+      home: '/'
+    },
     strategies: {
       local: {
         endpoints: {
