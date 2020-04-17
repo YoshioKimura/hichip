261d4751aab041d96ff7bdee1c02101984fb32c1
shiba mitsue <mitue.okamoto@gmail.com>
2020-04-17 12:10:26 +0900

    fix: login周りcomponent化

---
+++ components/UserAuthForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-form v-model="valid">
+    <v-text-field
+      v-model="userInfo.email"
+      :rules="[required('email')]"
+      label="メールアドレス"
+      name="email"
+    />
+    <v-text-field
+      v-model="userInfo.pw"
+      :type="showPw ? 'text' : 'password'"
+      :append-icon="showPw ? 'mdi-eye' : 'mdi-eye-off'"
+      :rules="[required('pw')]"
+      @click:append="showPw = !showPw"
+      counter
+      label="パスワード"
+      name="current-password"
+    />
+    <v-btn
+      @click="submitForm(userInfo)"
+      :disabled="!valid"
+    >
+      {{ btnText }}
+    </v-btn>
+  </v-form>
+</template>
+
+<script>
+import validations from '@/utils/validations'
+
+export default {
+  props: ['submitForm', 'btnText', 'hasName'],
+  data () {
+    return {
+      valid: false,
+      showPw: false,
+      userInfo: {
+        email: '',
+        pw: '',
+        name: ''
+      },
+      ...validations
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    click (type) {
+      alert(type)
+    },
+    async getPosts () {
+      this.posts = await this.$axios.$get(`/api/posts`, {}, {
+        headers: {
+          Authorization: localStorage.getItem('auth._token.local')
+        }
+      })
+    }
+  }
+}
+</script>
