13e8b5b96d7a04aeac981e07d81ccb327221c551
shiba mitsue <mitue.okamoto@gmail.com>
2020-04-17 15:48:05 +0900

    feat: superのモック追加

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -4,6 +4,7 @@
       <v-tab
         v-for="(tab, i) in tabs"
         :key="i"
+        @click="click(tab.type)"
       >
         {{ tab.label }}
       </v-tab>
@@ -16,11 +17,29 @@ export default {
   layout: 'admin',
   data () {
     return {
+      posts: [],
       tabs: [
         { label: '今年', type: 'year' },
-        { label: '今月', type: 'month' },
+        { label: '今月', type: 'mouth' },
         { label: '今週', type: 'week' }
       ]
+    }
+  },
+  mounted () {
+    this.getTransition('year')
+  },
+  methods: {
+    click (type) {
+      this.getTransition(type)
+    },
+    async getTransition (type) {
+      this.posts = await this.$axios.$post(`/api/chips/total_transition`, {
+        type
+      }, {
+        headers: {
+          Authorization: localStorage.getItem('auth._token.local')
+        }
+      })
     }
   }
 }
